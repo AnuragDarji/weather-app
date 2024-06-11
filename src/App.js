2728989e89b4d495b6ba5f3ccc5903e6c84c0525
AnuragDarji <anuragdarji29@gmail.com>
2024-06-11 21:33:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import WeatherApp from "./components/WeatherApp/WeatherApp";
+
+function App() {
+  return (
+    <div className="main-container">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
